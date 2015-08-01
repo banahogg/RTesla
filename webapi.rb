76c647a25ab1af434ee6c5434cb9b1523d154471
Eric Werness <ewerness@banahogg.com>
2015-07-31 21:01:47 -0700

    New authentication

--- webapi.rb
+++ webapi.rb
@@ -7,8 +7,8 @@ require 'uri'
 
 class Vehicle
   # Create a Vehicle with given API object and id
-  def initialize(cookies, prop)
-    @cookies = cookies
+  def initialize(token, prop)
+    @token = token
     @prop = prop
     @id = prop['id']
   end
@@ -17,10 +17,10 @@ class Vehicle
 
   # Low-level function to run a query
   def doQuery(command)
-    url = URI.parse("https://portal.vn.teslamotors.com/vehicles/#{@id}/#{command}")
+    url = URI.parse("https://owner-api.teslamotors.com/api/1/vehicles/#{@id}/#{command}")
 
     response = Net::HTTP.start(url.host, use_ssl: true, verify_mode: OpenSSL::SSL::VERIFY_NONE) do |http|
-      http.get(url.request_uri, { 'Cookie' => @cookies })
+      http.get(url.request_uri, { 'Authorization' => "Bearer #{@token}" })
     end
 
     p response if @debug
@@ -40,11 +40,11 @@ class Vehicle
 
   def awake() @prop['state'] == "online" end
   def mobileEnabled() doQuery('mobile_enabled') end
-  def chargeState() doQuery('command/charge_state') end
-  def climateState() doQuery('command/climate_state') end
-  def driveState() doQuery('command/drive_state') end
-  def vehicleState() doQuery('command/vehicle_state') end
-  def guiSettings() doQuery('command/gui_settings') end
+  def chargeState() doQuery('data_request/charge_state') end
+  def climateState() doQuery('data_request/climate_state') end
+  def driveState() doQuery('data_request/drive_state') end
+  def vehicleState() doQuery('data_request/vehicle_state') end
+  def guiSettings() doQuery('data_request/gui_settings') end
 
   def openChargePort() doCommand('command/charge_port_door_open') end
   def chargeStandard() doCommand('command/charge_standard') end
@@ -57,7 +57,7 @@ class Vehicle
   def doorLock() doCommand('command/door_lock') end
   def autoConditioningStart() doCommand('command/auto_conditioning_start') end
   def autoConditioningStop() doCommand('command/auto_conditioning_end') end
-  def wakeUp() doCommand('command/wake_up') end
+  def wakeUp() doCommand('wake_up') end
 
   def setTempsC(driver, passenger=driver)
     doCommand("command/set_temps?driver_temp=#{driver}&passengerTemp=#{passenger}")
@@ -72,31 +72,34 @@ class Vehicle
   def setChargeLimit(percent)
     doCommand("command/set_charge_limit?percent=#{percent}")
   end
+
+  def remoteStartDrive(password)
+    doCommand("command/remote_start_drive?password=#{password}")
+  end
 end
 
 # Basic read/write Model S web API
 class WebApi
 
-  # Takes username and password and generates login cookies
+  TeslaClientId = 'e4a9949fcfa04068f59abb5a658f2bac0a3428e4652315490b659d5ab3f35a9e'
+  TeslaClientSecret = 'c75f14bbadc8bee3a7594412c31416f8300256d7668ea7e6e7f06727bfb9d220'
+
   def login(username, password)
-    loginUrl = URI.parse('https://portal.vn.teslamotors.com/login')
+    loginUrl = URI.parse('https://owner-api.teslamotors.com/oauth/token')
 
     response = Net::HTTP.start(loginUrl.host, use_ssl: true, verify_mode: OpenSSL::SSL::VERIFY_NONE) do |http|
       request = Net::HTTP::Post.new(loginUrl.request_uri)
-      request.set_form_data({"user_session[email]" => username,
-                             "user_session[password]" => password})
-
+      request.set_form_data({"grant_type" => 'password',
+                             "client_id" => TeslaClientId,
+                             "client_secret" => TeslaClientSecret,
+                             "email" => username,
+                             "password" => password})
       http.request(request)
     end
 
-    throw "Login failed" unless response.code == '302'
+    throw "Login failed" unless response.code == '200'
 
-    all_cookies = response.get_fields('set-cookie')
-    cookies_array = Array.new
-    all_cookies.each { | cookie |
-      cookies_array.push(cookie.split('; ')[0])
-    }
-    @cookies = cookies_array.join('; ')
+    @token = JSON.parse(response.body)['access_token']
   end
   
   # Set debugging code on or off
@@ -109,34 +112,34 @@ class WebApi
   def initialize(username, password)
     @debug = false
     begin
-      @cookies = YAML::load(File.new("cookies.txt"))
+      @token = YAML::load(File.new("token.txt"))
       self.vehicles
     rescue
       self.login(username, password)
-      self.saveCookies
+      self.saveToken
     end
   end
   
   # Returns an array of vehicles, each of which is a hash
   # The id for for the commands below is in the "id" key of the vehicle
   def vehicles
-    vehiclesUrl = URI.parse('https://portal.vn.teslamotors.com/vehicles')
+    vehiclesUrl = URI.parse('https://owner-api.teslamotors.com/api/1/vehicles')
 
     response = Net::HTTP.start(vehiclesUrl.host, use_ssl: true, verify_mode: OpenSSL::SSL::VERIFY_NONE) do |http|
-      http.get(vehiclesUrl.request_uri, { 'Cookie' => @cookies })
+      http.get(vehiclesUrl.request_uri, { 'Authorization' => "Bearer #{@token}" })
     end
 
     throw "Can't load vehicles" unless response.code == '200'
 
     vehicles = JSON.parse(response.body)
-    
-    return vehicles.map {|x| Vehicle.new(@cookies, x)}
+
+    return vehicles['response'].map {|x| Vehicle.new(@token, x)}
   end
 
-  # Save the current login cookies to a file "cookies.txt"
-  def saveCookies
-    File.open("cookies.txt", "w") do |f|
-      f.write(@cookies.to_yaml)
+  # Save the current login tokens to a file "token.txt"
+  def saveToken
+    File.open("token.txt", "w") do |f|
+      f.write(@token.to_yaml)
     end
   end
 end
@@ -148,10 +151,13 @@ if $0 == __FILE__
 
   vehicles = api.vehicles
   p vehicles[0]
+  p vehicles[0].mobileEnabled
   p vehicles[0].awake
-#  p vehicles[0].mobileEnabled
-#  p vehicles[0].chargeState
-#  p vehicles[0].climateState
-#  p vehicles[0].driveState
-#  p vehicles[0].vehicleState
+  if vehicles[0].awake
+    p vehicles[0].mobileEnabled
+    p vehicles[0].chargeState
+    p vehicles[0].climateState
+    p vehicles[0].driveState
+    p vehicles[0].vehicleState
+  end
 end
